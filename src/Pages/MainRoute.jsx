f5be20e7503ae7674a0df8df885d707c6ace1eeb
raushan kuamr <sraushan890@gmail.com>
2022-06-30 15:09:40 +0530

    update data

--- src/Pages/MainRoute.jsx
+++ src/Pages/MainRoute.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import{Routes,Route} from "react-router-dom"
-import Admin from './Admin'
+import Login from './Login'
 import Books from './Books'
 import EditBooks from './EditBooks'
 import SingleBooks from './SingleBooks'
@@ -10,7 +10,7 @@ function MainRoute() {
           <Route path='/' element={<Books/>}/>
           <Route path='/books/:id'element={<SingleBooks/>}/>
           <Route path='/books/:id/edit' element={<EditBooks/>}/>
-          <Route path='/admin' element={<Admin/>}/>
+          <Route path='/admin' element={<Login/>}/>
           <Route path='*' element={<h2>page is not found</h2>}/>
     </Routes>
   )
